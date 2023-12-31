1641c2670c58e238e50e804b84f305ff02ada79d
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2023-12-31 18:49:05 +0100

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 import io
 
 # Set Streamlit page configuration
@@ -52,25 +51,19 @@ df = data[data['REVENUE'].notnull() & (data['REVENUE'] != '')]
 df.loc[:, 'REVENUE'] = pd.to_numeric(df['REVENUE'], errors='coerce')
 revenue_by_channel = df.groupby('MARKETINGCHANNEL')['REVENUE'].sum().reset_index()
 
-# Set custom color palette for Seaborn plots
-custom_palette = "coolwarm"  # Using a named palette
-sns.set_palette(custom_palette)
-
 # Plotting Revenue Attribution by Marketing Channel
 col1, col2 = st.columns(2)
 with col1:
     st.subheader('Revenue Attribution by Marketing Channel')
-    plt.figure(figsize=(10, 6))
-    plt.pie(revenue_by_channel['REVENUE'], labels=revenue_by_channel['MARKETINGCHANNEL'], autopct='%1.1f%%', startangle=90)
-    st.pyplot()
+    fig = px.pie(revenue_by_channel, values='REVENUE', names='MARKETINGCHANNEL', title='Revenue Attribution by Marketing Channel')
+    st.plotly_chart(fig)
 
 # Plotting Distribution of Customers per Marketing Channel as a Pie Chart
 with col2:
     st.subheader('Distribution of Customers per Marketing Channel')
     customer_dist_by_channel = data.groupby('MARKETINGCHANNEL')['CUSTOMERID'].nunique().sort_values(ascending=False).reset_index()
-    plt.figure(figsize=(10, 6))
-    plt.pie(customer_dist_by_channel['CUSTOMERID'], labels=customer_dist_by_channel['MARKETINGCHANNEL'], autopct='%1.1f%%', startangle=90)
-    st.pyplot()
+    fig = px.pie(customer_dist_by_channel, values='CUSTOMERID', names='MARKETINGCHANNEL', title='Distribution of Customers per Marketing Channel')
+    st.plotly_chart(fig)
 
 st.info("""
 The pie charts show revenue distribution and customer distribution by marketing channel. The top revenue channels are Direct NON-BRAND and SEO_BRAND, while the top customer channels are Direct NON-BRAND and SEO_BRAND.
@@ -78,14 +71,9 @@ The pie charts show revenue distribution and customer distribution by marketing 
 
 # Time Series Analysis of Touchpoints
 st.subheader('Time Series Analysis of Touchpoints (Daily)')
-plt.figure(figsize=(11, 7))
-data['TIMESTAMP_TOUCHPOINT'] = pd.to_datetime(data['TIMESTAMP_TOUCHPOINT'], errors='coerce')
-data.set_index('TIMESTAMP_TOUCHPOINT').resample('D').size().plot()
-plt.axvline(x=pd.Timestamp('2019-12-23'), linestyle="--", linewidth=2, color="green")
-plt.axvline(x=pd.Timestamp('2020-01-07'), linestyle="--", linewidth=2, color="green")
-plt.xlabel('Date')
-plt.ylabel('Number of Touchpoints')
-st.pyplot()
+fig = px.line(data, x='TIMESTAMP_TOUCHPOINT', title='Number of Touchpoints Over Time (Daily)')
+fig.add_shape(dict(type='line', x0='2019-12-23', x1='2020-01-07', y0=0, y1=1, line=dict(color='green', width=2, dash='dash')))
+st.plotly_chart(fig)
 
 st.info("""
 The line chart shows the number of touchpoints over time. As you can see, the touchpoints increase significantly around the Christmas holiday (between 23 December and 7 January).
@@ -95,36 +83,21 @@ The line chart shows the number of touchpoints over time. As you can see, the to
 st.subheader('Distribution of Touchpoints by Year/Month/Hour of Day')
 data['TIMESTAMP_TOUCHPOINT'] = pd.to_datetime(data['TIMESTAMP_TOUCHPOINT'], errors='coerce')
 
-# Extracting date components from 'TIMESTAMP_TOUCHPOINT'
-data['YEAR'] = data['TIMESTAMP_TOUCHPOINT'].dt.year
-data['MONTH'] = data['TIMESTAMP_TOUCHPOINT'].dt.month
-data['DAY'] = data['TIMESTAMP_TOUCHPOINT'].dt.day
-data['HOUR'] = data['TIMESTAMP_TOUCHPOINT'].dt.hour
-data['WEEKDAY'] = data['TIMESTAMP_TOUCHPOINT'].dt.day_name()
-
-time_component = st.selectbox("Choose time component:", ["Year", "Month", "Hour", "WeekDay"])
+time_component = st.selectbox("Choose time component:", ["Year", "Month", "Hour", "Weekday"])
 
 # Analyzing distribution of touchpoints over different time components
 if time_component == "Year":
-    fig, ax = plt.subplots(figsize=(11, 7))
-    sns.countplot(x='YEAR', data=data, ax=ax)
-    ax.set_title('Distribution of Touchpoints by Year')
-    st.pyplot(fig)
+    fig = px.histogram(data, x='YEAR', title='Distribution of Touchpoints by Year')
+    st.plotly_chart(fig)
 elif time_component == "Month":
-    fig, ax = plt.subplots(figsize=(11, 7))
-    sns.countplot(x='MONTH', data=data, ax=ax)
-    ax.set_title('Distribution of Touchpoints by Month')
-    st.pyplot(fig)
+    fig = px.histogram(data, x='MONTH', title='Distribution of Touchpoints by Month')
+    st.plotly_chart(fig)
 elif time_component == "Hour":
-    fig, ax = plt.subplots(figsize=(11, 7))
-    sns.countplot(x='HOUR', data=data, ax=ax)
-    ax.set_title('Distribution of Touchpoints by Hour of Day')
-    st.pyplot(fig)
-elif time_component == "WeekDay":
-    fig, ax = plt.subplots(figsize=(11, 7))
-    sns.countplot(x='WEEKDAY', data=data, ax=ax)
-    ax.set_title('Distribution of Touchpoints by Day of the week')
-    st.pyplot(fig)
+    fig = px.histogram(data, x='HOUR', title='Distribution of Touchpoints by Hour of Day')
+    st.plotly_chart(fig)
+elif time_component == "Weekday":
+    fig = px.histogram(data, x='WEEKDAY', title='Distribution of Touchpoints by Day of the week')
+    st.plotly_chart(fig)
 
 st.info("""
 - Customer engagement is highest during the end of the year.
@@ -132,30 +105,19 @@ st.info("""
 - Customer engagement is highest during the weekend.
 """)
 
-# Analyzing the number of sessions per customer"
+# Analyzing the number of sessions per customer
 sessions_per_customer = data.groupby('CUSTOMERID')['SESSIONID'].nunique().sort_values(ascending=False)
 
 st.subheader('Distribution of Number of Sessions per Customer')
 
-# Plotting with Matplotlib
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.hist(sessions_per_customer, bins=50, edgecolor='black', alpha=0.7)
-
-ax.set_xlim(0, 10)
-
-ax.set_title('Distribution of Number of Sessions per Customer')
-ax.set_xlabel('Number of Sessions')
-ax.set_ylabel('Frequency')
-
-# Display the plot in Streamlit
-st.pyplot(fig)
+# Plotting with Plotly Express
+fig = px.histogram(sessions_per_customer, x='SESSIONID', nbins=50, title='Distribution of Number of Sessions per Customer')
+fig.update_layout(xaxis=dict(range=[0, 10]), title='Distribution of Number of Sessions per Customer', xaxis_title='Number of Sessions', yaxis_title='Frequency')
+st.plotly_chart(fig)
 
 st.info("The histogram indicates a high concentration of customers with only one session, suggesting that a majority of the customer base might be engaging in one-time interactions. This finding can be pivotal for strategies focusing on customer retention and repeated engagement.")
-
-
 
 st.header("References:")
 st.markdown("- **Streamlit Documentation:** [Streamlit Docs](https://docs.streamlit.io/)")
 st.markdown("- **Pandas Documentation:** [Pandas Docs](https://pandas.pydata.org/pandas-docs/stable/)")
-st.markdown("- **Matplotlib Documentation:** [Matplotlib Docs](https://matplotlib.org/stable/contents.html)")
-st.markdown("- **Seaborn Documentation:** [Seaborn Docs](https://seaborn.pydata.org/)")
+st.markdown("- **Plotly Express Documentation:** [Plotly Express Docs](https://plotly.com/python/plotly-express/)")
